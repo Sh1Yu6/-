4ab32c2546950ecd80c9455f920eb6795968784c
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2021-04-21 09:25:39 +0800

    a

--- student_infomation_management_system/main.cpp
+++ student_infomation_management_system/main.cpp
@@ -6,7 +6,7 @@
 #        Author: Sh1Yu6
 #   Description: ---
 #        Create: 2021-03-12 11:06:28
-# Last Modified: 2021-03-12 12:30:23
+# Last Modified: 2021-04-20 23:27:07
 #***********************************************/
 #include <iostream>
 #include "primary_student.h"
